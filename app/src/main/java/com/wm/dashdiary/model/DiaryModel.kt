055f37c86ed9c3fbec0a88de441d384806bd2aa6
Dykyi Maksym <108894931+DykyiMaxim@users.noreply.github.com>
2023-02-27 22:39:18 -0400

    Design the moods for diary's and add them to enum class,Make Diary model for realm and design DiaryHolder Date design

---
+++ app/src/main/java/com/wm/dashdiary/model/DiaryModel.kt
@@ -0,0 +1,20 @@
+package com.wm.dashdiary.model
+
+import io.realm.kotlin.ext.realmListOf
+import io.realm.kotlin.types.ObjectId
+import io.realm.kotlin.types.RealmInstant
+import io.realm.kotlin.types.RealmList
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.PrimaryKey
+
+class DiaryModel:RealmObject {
+    @PrimaryKey
+    var Id: ObjectId = ObjectId.create()
+    var OwnerId: String = ""
+    var title: String = ""
+    var mood:String=Mood.Neutral.name
+    var description: String = ""
+    var Images: RealmList<String> = realmListOf()
+    var date: RealmInstant = RealmInstant.from(System.currentTimeMillis(), 0)
+}
+
