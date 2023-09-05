15e4b11eaf343aba3287a8028186d8ae0277bd7c
Dykyi Maksym <108894931+DykyiMaxim@users.noreply.github.com>
2023-09-05 21:30:26 +0100

    Add Diary Holder and start configuration Firebase for images store

--- app/src/main/java/com/wm/dashdiary/presentation/components/DiaryHolder.kt
+++ app/src/main/java/com/wm/dashdiary/presentation/components/DiaryHolder.kt
@@ -42,7 +42,7 @@ fun DiaryHolder(diary: Diary, onClick: (String) -> Unit) {
         .clickable(
             indication = null,
             interactionSource = remember { MutableInteractionSource() })
-        { onClick(diary.Id.toString()) }) {
+        { onClick(diary._id.toString()) }) {
 
         Spacer(modifier = Modifier.width(14.dp))
         Surface(
