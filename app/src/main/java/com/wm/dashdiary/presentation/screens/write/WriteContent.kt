1f25f8285d8b01390dea00f9e056279d9507153a
Dykyi Maksym <108894931+DykyiMaxim@users.noreply.github.com>
2023-09-12 23:57:42 +0100

    Designing WriteScreen content.

---
+++ app/src/main/java/com/wm/dashdiary/presentation/screens/write/WriteContent.kt
@@ -0,0 +1,41 @@
+package com.wm.dashdiary.presentation.screens.write
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WriteContent(
+    paddingValues: PaddingValues
+) {
+    var scrollState = rememberScrollState()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = paddingValues.calculateTopPadding())
+            .padding(bottom = paddingValues.calculateBottomPadding())
+            .padding(bottom = 24.dp)
+            .padding(horizontal = 24.dp),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .verticalScroll(scrollState)
+        ) {
+            Spacer(modifier = Modifier.height(30.dp))
+
+        }
+
+    }
+
+}
