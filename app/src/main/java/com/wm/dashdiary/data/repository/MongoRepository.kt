73d575096f3f5e85d3c34f8a0763c2e02d336782
Dykyi Maksym <108894931+DykyiMaxim@users.noreply.github.com>
2023-09-23 18:40:30 -0200

    Implement functionality to filter diaries.

--- app/src/main/java/com/wm/dashdiary/data/repository/MongoRepository.kt
+++ app/src/main/java/com/wm/dashdiary/data/repository/MongoRepository.kt
@@ -4,6 +4,7 @@ import com.wm.dashdiary.model.Diary
 import kotlinx.coroutines.flow.Flow
 import org.mongodb.kbson.ObjectId
 import java.time.LocalDate
+import java.time.ZonedDateTime
 
 typealias Diaries = RequestState<Map<LocalDate, List<Diary>>>
 
@@ -11,6 +12,7 @@ interface MongoRepository {
     fun configureRealm()
     fun getAllDiaries(): Flow<Diaries>
     fun getDiaryById(diaryID: ObjectId): Flow<RequestState<Diary>>
+    fun getFilteredDiaries(zonedDateTime: ZonedDateTime): Flow<Diaries>
     suspend fun insertDiary(diary: Diary): RequestState<Diary>
     suspend fun updateDiary(diary: Diary): RequestState<Diary>
     suspend fun deleteDiary(id: ObjectId): RequestState<Diary>
